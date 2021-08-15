d4317dec8323d1595019c137c9fea98647c67c51
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-08-15 16:53:16 +0900

    add bcrypt and loginmanager

--- tornado/__init__.py
+++ tornado/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('tornado.config')
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+# login_manager.login_view = 'login'
 
 from tornado import routes
