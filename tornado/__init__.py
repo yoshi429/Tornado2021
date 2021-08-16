9e75d979ee0a8499190cb5892ec39227970c30b1
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-08-16 17:23:08 +0900

    ログイン設定追加

--- tornado/__init__.py
+++ tornado/__init__.py
@@ -8,6 +8,8 @@ app.config.from_object('tornado.config')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-# login_manager.login_view = 'login'
+login_manager.login_view = 'user_login'
+
+
 
 from tornado import routes
