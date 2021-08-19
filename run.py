eead62d15885fba1e1abf50e14e8845a63264807
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-08-20 07:32:06 +0900

    管理設定追加

--- run.py
+++ run.py
@@ -2,17 +2,21 @@ from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from tornado import app, db
-from tornado.models import User, Profile, Post, PostChild, Good, Comment
+from tornado.models import User, Profile, Post, PostChild, Good, Comment, AdminUser
+from tornado.admin.routes import MyAdminIndexView, MyModelView
+
 
 migrate = Migrate(app, db)
 
-admin = Admin(app)
+# 管理設定
+admin = Admin(app, '管理者画面', index_view=MyAdminIndexView(), base_template='admin/my_master.html')
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Profile, db.session))
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(PostChild, db.session))
 admin.add_view(ModelView(Good, db.session))
 admin.add_view(ModelView(Comment, db.session))
+admin.add_view(MyModelView(AdminUser, db.session))
 
 if __name__ == '__main__':
     app.run(debug=True)
