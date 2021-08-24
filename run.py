3f44adf0561808324439519ccc39c81a95a31770
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-08-24 14:28:36 +0900

    管理者権限機能追加

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from flask_migrate import Migrate
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 from tornado import app, db
 from tornado.models import User, Profile, Post, PostChild, Comment, Category, Tag
-# from tornado.admin.routes import MyAdminIndexView, MyModelView
+from tornado.admin.routes import MyModelView
 
 
 migrate = Migrate(app, db)
@@ -11,14 +10,14 @@ migrate = Migrate(app, db)
 # 管理設定
 # admin = Admin(app, '管理者画面', index_view=MyAdminIndexView(), base_template='admin/my_master.html')
 admin = Admin(app)
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Profile, db.session))
-admin.add_view(ModelView(Category, db.session))
-admin.add_view(ModelView(Tag, db.session))
-admin.add_view(ModelView(Post, db.session))
-admin.add_view(ModelView(PostChild, db.session))
-admin.add_view(ModelView(Comment, db.session))
-# admin.add_view(MyModelView(AdminUser, db.session))
+admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(Profile, db.session))
+admin.add_view(MyModelView(Category, db.session))
+admin.add_view(MyModelView(Tag, db.session))
+admin.add_view(MyModelView(Post, db.session))
+admin.add_view(MyModelView(PostChild, db.session))
+admin.add_view(MyModelView(Comment, db.session))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tornado/admin/routes.py
+++ tornado/admin/routes.py
@@ -1,58 +1,7 @@
-from flask import url_for, redirect, request
-import flask_admin as admin
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user
 from flask_admin.contrib import sqla
-from flask_admin import helpers, expose
-
-from tornado import db, bcrypt
-from tornado.models import User
-from tornado.admin.forms import LoginForm, RegistrationForm
 
 
 class MyModelView(sqla.ModelView):
     def is_accessible(self):
-        return current_user.is_authenticated
-
-
-class MyAdminIndexView(admin.AdminIndexView):
-    @expose('/admin')
-    def index(self):
-        if not current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-        return super(MyAdminIndexView, self).index()
-
-    @expose('admin/login/', methods=('GET', 'POST'))
-    def login_view(self):
-        form = LoginForm(request.form)
-        if helpers.validate_form_on_submit(form):
-            user = form.get_user()
-            login_user(user)
-
-        if current_user.is_authenticated:
-            return redirect(url_for('.index'))
-        link = '<p>アカウント未作成用 <a href="' + url_for('.register_view') + '">ここをクリック</a></p>'
-        self._template_args['form'] = form
-        self._template_args['link'] = link
-        return super(MyAdminIndexView, self).index()
-
-    @expose('admin/register/', methods=('GET', 'POST'))
-    def register_view(self):
-        form = RegistrationForm(request.form)
-        if helpers.validate_form_on_submit(form):
-            user = User()
-
-            form.populate_obj(user)
-            user.password = bcrypt.generate_password_hash(form.password.data)
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            return redirect(url_for('.index'))
-        link = '<p>既にアカウントを持っている場合は <a href="' + url_for('.login_view') + '">ここをクリックしてログイン</a></p>'
-        self._template_args['form'] = form
-        self._template_args['link'] = link
-        return super(MyAdminIndexView, self).index()
-
-    @expose('admin/logout/')
-    def logout_view(self):
-        logout_user()
-        return redirect(url_for('.index'))
+        return current_user.is_authenticated and current_user.is_admin
