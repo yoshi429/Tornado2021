1efbc3b69f83ee4884c256873b4bc86bb3050876
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-08-28 08:43:00 +0900

    adjust savepicures

--- run.py
+++ run.py
@@ -20,5 +20,5 @@ admin.add_view(MyModelView(Comment, db.session))
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port='5000')
