de9f1de5e93dfe7bc61a5cc455d3a880df6f01ef
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-08-31 21:46:02 +0900

    remove aws information

--- tornado/config.py
+++ tornado/config.py
@@ -2,6 +2,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    AWS_ACCESS_KEY_ID = "AKIAZYUKYYMT47ZPADVE"
-    AWS_SECRET_ACCESS_KEY = "bXENZCd+Qs3QVq9BWhwFqovaLzyePLeYA7a2g1xP"
-    S3_BUCKET = "tornado2021"
+    AWS_ACCESS_KEY_ID = "" # your aws access key id
+    AWS_SECRET_ACCESS_KEY = "" # your aws secret access key
+    S3_BUCKET = "" # youtr S3 Bucket Name
